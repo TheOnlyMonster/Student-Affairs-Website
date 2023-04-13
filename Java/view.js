bd2077fa71797291d249c9e1a5a3aacf6c6039fe
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2023-04-13 13:05:32 +0200

    add show student in view.js

--- Java/view.js
+++ Java/view.js
@@ -1,3 +1,4 @@
+/*
 function activateButton() {
     const activeButton = document.getElementById('Active-button');
     const inactiveButton = document.getElementById('inactive-Button');
@@ -27,7 +28,46 @@ function filterSearch(){
 }
 activateButton();
 filterSearch();
+*/
+window.onload = function() {
+   showStudent(JSON.parse(localStorage.getItem("students")));
 
+  };
+function showStudent(students) {
+    let tableBody = document.getElementById("table-body");
+    students.forEach((student) => {  
+        let row = document.createElement("tr");
+        let id = document.createElement("td");
+        let FullName = document.createElement("td");
+        let GPA = document.createElement("td");
+        let department = document.createElement("td");
+        let active = document.createElement("button");
+        let inactive = document.createElement("button");
+    
+        id.innerText = student.id;
+        FullName.innerHTML = `<a href="department.html">${student.Fname} ${student.Lname}</a>`;
+        GPA.innerText = student.GPA;
+        department.innerText=student.department
+        active.innerText = "Active";
+        active.classList.add('btn');
+        inactive.innerText = "Inactive";
+        inactive.classList.add('btn');
+        if (student.Status === "Active"){
+            active.classList.add('active');
+        }
+        else if(student.Status === "Inactive"){
+            inactive.classList.add('active');
+        }
+        
+        row.appendChild(id);
+        row.appendChild(FullName);
+        row.appendChild(GPA);
+        row.appendChild(department);
+        row.appendChild(active);
+        row.appendChild(inactive);
+        tableBody.appendChild(row)
+    });
+}
 
 
 
