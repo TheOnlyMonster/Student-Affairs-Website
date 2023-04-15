88c7eab5256b5fd3b4fed3960519ae76fdd7ac35
Abdelrahman Adel <abdelrahmanadel203@gmail.com>
2023-04-15 23:15:00 +0200

    Added Department th Element on View Active Table

--- Java/view-active.js
+++ Java/view-active.js
@@ -2,11 +2,28 @@ let AllStudents = JSON.parse(localStorage.getItem("students"));
 let activeStudents = AllStudents.filter((ele) => ele.Status === "Active");
 let tableBody = document.body.querySelector("table tbody");
 
+function filterSearch() {
+  const searchInput = document.getElementById("search");
+  const table = document.getElementById("table");
+  searchInput.addEventListener("keyup", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    Array.from(table.querySelectorAll("tbody tr")).forEach((row) => {
+      const name = row.cells[1].textContent.toLowerCase();
+      if (name.includes(searchTerm)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  });
+}
+
 activeStudents.forEach((element) => {
   let row = document.createElement("tr");
   let stdID = document.createElement("td");
   let stdName = document.createElement("td");
   let stdGPA = document.createElement("td");
+  let stdDept = document.createElement("td");
   let stdModify = document.createElement("td");
   let btn1 = document.createElement("a");
   let btn2 = document.createElement("a");
@@ -14,6 +31,7 @@ activeStudents.forEach((element) => {
   stdID.textContent = element.id;
   stdName.textContent = `${element.Fname} ${element.Lname}`;
   stdGPA.textContent = element.GPA;
+  stdDept.textContent = element.department;
   btn1.textContent = "Assign Department";
   btn1.href = `assign-dept.html?id=${element.id}`;
   btn2.textContent = "Student Details";
@@ -23,31 +41,8 @@ activeStudents.forEach((element) => {
   row.appendChild(stdID);
   row.appendChild(stdName);
   row.appendChild(stdGPA);
+  row.appendChild(stdDept);
   row.appendChild(stdModify);
   tableBody.appendChild(row);
 });
 
-{
-  /* <table class="container table-container">
-    <thead>
-        <th>ID</th>
-        <th>Name</th>
-        <th>GPA</th>
-        <th>Modify Student</th>
-    </thead>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>
-                <a>Abdelrahman Adel</a>
-            </td>
-            <td>3.78</td>
-            <td>
-                    <a href="assign-dept.html">Assign Department</a>
-                    <a href="edit-std.html">Edit Student Details</a>
-            </td>
-        </tr>
-        
-    </tbody>
-</table> */
-}
