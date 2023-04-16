9a398707145c6471c68188c908a6060ea2f82390
Abdelrahman Adel <abdelrahmanadel203@gmail.com>
2023-04-16 07:08:44 +0200

    User can now delete a student and change his department depending on his level  + Added sweet confirmation dialogues upon each change on a student details page & assign department page

--- Java/assign-dept.js
+++ Java/assign-dept.js
@@ -1,31 +1,67 @@
 let myForm = document.querySelector("form");
-let id = window.location.search
+let std;
+let stdID = window.location.search
   .split("")
   .filter((ele) => !isNaN(ele))
   .join("");
-document.getElementById("id").value = id;
 let students = JSON.parse(localStorage.getItem("students"));
-students.forEach((element) => {
-  if (element.id == id) {
-    document.getElementById("Phone").value = element.Phone;
-    document.getElementById("Address").value = element.Address;
-    document.getElementById("Fname").value = element.Fname;
-    document.getElementById("Lname").value = element.Lname;
-    document.getElementById("Nid").value = element.Nid;
-    document.getElementById("Email").value = element.Email;
-    document.getElementById("Level").value = element.Level;
-    document.getElementById("Gender").value = element.Gender;
-    document.getElementById("Status").value = element.Status;
-    let currDate = new Date();
-    let stdDate = new Date(element.Birthday);
-    let stdAge = currDate.getFullYear() - stdDate.getFullYear();
-    document.getElementById("Birthday").value = stdAge;
-    document.getElementById("GPA").value = element.GPA;
-    document.getElementById("department").value = element.department;
-    myForm.onsubmit = function () {
-      let stdDept = document.querySelector("select").value;
-      element.department = stdDept;
-      localStorage.setItem("students", JSON.stringify(students));
-    };
+for (let i = 0; i < students.length; i++) {
+  if (students[i].id == stdID) {
+    std = students[i];
+    break;
   }
-});
+}
+document.getElementById("id").value = std.id;
+document.getElementById("Phone").value = std.Phone;
+document.getElementById("Address").value = std.Address;
+document.getElementById("Fname").value = std.Fname;
+document.getElementById("Lname").value = std.Lname;
+document.getElementById("Nid").value = std.Nid;
+document.getElementById("Email").value = std.Email;
+document.getElementById("Level").value = std.Level;
+document.getElementById("Gender").value = std.Gender;
+document.getElementById("Status").value = std.Status;
+let currDate = new Date();
+let stdDate = new Date(std.Birthday);
+let stdAge = currDate.getFullYear() - stdDate.getFullYear();
+document.getElementById("Birthday").value = stdAge;
+document.getElementById("GPA").value = std.GPA;
+document.getElementById("department").value = std.department;
+if (std.Status === "Active" && std.Level >= 3) {
+  myForm.onsubmit = function (e) {
+    e.preventDefault();
+    let stdDept = document.querySelector("select").value;
+    if (std.department != stdDept) {
+      Swal.fire({
+        title: `Do you want to change department for student ${std.id} from ${std.department} to ${stdDept}?`,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          std.department = stdDept;
+          localStorage.setItem("students", JSON.stringify(students));
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+    }
+  };
+} else {
+  document.getElementById("department").setAttribute("disabled", true);
+  if (std.Status === "Inactive") {
+    showError("Status", "Student should be active.");
+  }
+  if (std.Level < 3) {
+    showError("Level", "Student level must be greater than or equal to 3.");
+  }
+}
+function showError(ele, msg) {
+  document.getElementById(`${ele}`).style.border = `3px solid red`;
+  let errorMsg = document.createElement("div");
+  errorMsg.textContent = msg;
+  errorMsg.id = `input-error`;
+  document.getElementById(`${ele}`).after(errorMsg);
+}
--- Java/edit-std.js
+++ Java/edit-std.js
@@ -1,80 +1,112 @@
 let currDate = new Date();
 let myStyle = document.getElementById("Nid").style;
 let students = JSON.parse(localStorage.getItem("students"));
+let std;
 let stdID = location.search
   .split("")
   .filter((ele) => !isNaN(ele))
   .join("");
-students.forEach((element) => {
-  if (element.id == stdID) {
-    document.getElementById("Phone").value = element.Phone;
-    document.getElementById("Address").value = element.Address;
-    document.getElementById("Fname").value = element.Fname;
-    document.getElementById("Lname").value = element.Lname;
-    document.getElementById("Nid").value = element.Nid;
-    document.getElementById("Email").value = element.Email;
-    document.getElementById("Level").value = element.Level;
-    document.getElementById("Gender").value = element.Gender;
-    document.getElementById("Status").value = element.Status;
-    document.getElementById("Birthday").value = element.Birthday;
-    document.getElementById("GPA").value = element.GPA;
-    document.getElementById("department").value = element.department;
-
-    document.getElementById("Fname").onkeydown = validName;
-    document.getElementById("Lname").onkeydown = validName;
-    document.body.querySelector("form").onsubmit = function (e) {
-      e.preventDefault();
-      if (document.getElementById(`input-error`) != null) {
-        document.getElementById(`input-error`).previousElementSibling.style =
-          myStyle;
-        document.getElementById(`input-error`).remove();
-      }
-      if (document.getElementById("Nid").value.length != 14) {
-        showError("Nid", "Please enter 14 digits.");
-        return;
-      }
-      if (document.getElementById("Phone").value.length != 11) {
-        showError("Phone", "Please enter 11 digits.");
-        return;
-      }
-      let stdDate = new Date(document.getElementById("Birthday").value);
-      if (currDate.getFullYear() - stdDate.getFullYear() < 17) {
-        showError("Birthday", "Your age must be greater than 17.");
-        return;
-      }
-
-      for (let i = 0; i < students.length; i++) {
-        if (
-          students[i].Nid === document.getElementById("Nid").value &&
-          students[i].id != element.id
-        ) {
-          showError("Nid", "National ID already taken.");
-          return;
-        }
-      }
-      element.Fname = document.getElementById("Fname").value;
-      element.Lname = document.getElementById("Lname").value;
-      element.Nid = document.getElementById("Nid").value;
-      element.Email = document.getElementById("Email").value;
-      element.Phone = document.getElementById("Phone").value;
-      element.Address = document.getElementById("Address").value;
-      element.Birthday = document.getElementById("Birthday").value;
-      element.Gender = document.getElementById("Gender").value;
-      element.Status = document.getElementById("Status").value;
-      element.Level = document.getElementById("Level").value;
-      element.GPA = document.getElementById("GPA").value;
-
-      localStorage.setItem("students", JSON.stringify(students));
-      let myDiv = document.createElement("div");
-      myDiv.textContent = `Student info for ${element.id} has been changed.`;
-      myDiv.style.fontSize = `13px`;
-      document.querySelector(`input[type="submit"]`).before(myDiv);
-      setTimeout(function () {
-        myDiv.remove();
-      }, 3000);
-    };
+for (let i = 0; i < students.length; i++) {
+  if (students[i].id == stdID) {
+    std = students[i];
+    break;
   }
-});
+}
+document.getElementById("Phone").value = std.Phone;
+document.getElementById("Address").value = std.Address;
+document.getElementById("Fname").value = std.Fname;
+document.getElementById("Lname").value = std.Lname;
+document.getElementById("Nid").value = std.Nid;
+document.getElementById("Email").value = std.Email;
+document.getElementById("Level").value = std.Level;
+document.getElementById("Gender").value = std.Gender;
+document.getElementById("Status").value = std.Status;
+document.getElementById("Birthday").value = std.Birthday;
+document.getElementById("GPA").value = std.GPA;
+document.getElementById("department").value = std.department;
+
+document.getElementById("Fname").onkeydown = validName;
+document.getElementById("Lname").onkeydown = validName;
+
+document.getElementById("delete").addEventListener("click", deleteStd);
+
+function deleteStd(e) {
+  e.preventDefault();
+  Swal.fire({
+    title: `Are you sure you want to delete student ${std.id}?`,
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#002a56",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      students.splice(students.indexOf(std), 1);
+      localStorage.setItem("students", JSON.stringify(students));
+      Swal.fire("Deleted!", `Student ${std.id} has been deleted.`, "success");
+      setTimeout(function () {
+        window.location.pathname = "view.html";
+      }, 2000);
+    }
+  });
+}
+document.getElementById("change").onclick = function (e) {
+  e.preventDefault();
+  if (document.getElementById(`input-error`) != null) {
+    document.getElementById(`input-error`).previousElementSibling.style =
+      myStyle;
+    document.getElementById(`input-error`).remove();
+  }
+  if (document.getElementById("Nid").value.length != 14) {
+    showError("Nid", "Please enter 14 digits.");
+    return;
+  }
+  if (document.getElementById("Phone").value.length != 11) {
+    showError("Phone", "Please enter 11 digits.");
+    return;
+  }
+  let stdDate = new Date(document.getElementById("Birthday").value);
+  if (currDate.getFullYear() - stdDate.getFullYear() < 17) {
+    showError("Birthday", "Your age must be greater than 17.");
+    return;
+  }
+
+  for (let i = 0; i < students.length; i++) {
+    if (
+      students[i].Nid === document.getElementById("Nid").value &&
+      students[i].id != std.id
+    ) {
+      showError("Nid", "National ID already taken.");
+      return;
+    }
+  }
+  Swal.fire({
+    title: "Do you want to save the changes?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Save",
+    denyButtonText: `Don't save`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      std.Fname = document.getElementById("Fname").value;
+      std.Lname = document.getElementById("Lname").value;
+      std.Nid = document.getElementById("Nid").value;
+      std.Email = document.getElementById("Email").value;
+      std.Phone = document.getElementById("Phone").value;
+      std.Address = document.getElementById("Address").value;
+      std.Birthday = document.getElementById("Birthday").value;
+      std.Gender = document.getElementById("Gender").value;
+      std.Status = document.getElementById("Status").value;
+      std.Level = document.getElementById("Level").value;
+      std.GPA = document.getElementById("GPA").value;
+      localStorage.setItem("students", JSON.stringify(students));
+      Swal.fire("Saved!", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Changes are not saved", "", "info");
+    }
+  });
+};
 
 function validName(e) {
   if (!e.key.match(`[a-z]|[A-Z]`)) {
